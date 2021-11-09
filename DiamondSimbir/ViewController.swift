462160f8bbe0d5989210f971ff07165d3ed9784b
Evgeniy <gafter921@yandex.ru>
2021-11-09 22:10:11 +0300

    Home work

--- DiamondSimbir/ViewController.swift
+++ DiamondSimbir/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelBottom: UILabel!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        labelBottom.text = ""
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func ImagButton(_ sender: Any) {
+        labelBottom.text = "Diamond!" 
+    }
+    
 
 }
 
